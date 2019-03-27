d74518aae0babf5dca69bfd356b6201ecf06d10c
Alice <u6499267@anu.edu.au>
2019-03-27 13:52:33 +1100

    Saving Progress

---
+++ src/comp1110/ass2/gui/gittest/A.java
@@ -0,0 +1,7 @@
+package comp1110.ass2.gui.gittest;
+
+public class A {
+    public static void main(String[] args) {
+        toString();
+    }
+}
---
+++ src/comp1110/ass2/gui/gittest/Main.java
@@ -0,0 +1,7 @@
+package comp1110.ass2.gui.gittest;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
