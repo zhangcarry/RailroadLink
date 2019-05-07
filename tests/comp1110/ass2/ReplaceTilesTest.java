4d21fdaf5ab4ed87bfb08db0c5e74203446040a4
Qixia Lu <u6805636@anu.edu.au>
2019-05-07 11:40:46 +1000

    save progress

--- tests/comp1110/ass2/ReplaceTilesTest.java
+++ tests/comp1110/ass2/ReplaceTilesTest.java
@@ -4,6 +4,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import static junit.framework.TestCase.assertEquals;
@@ -11,9 +12,8 @@ import static junit.framework.TestCase.assertTrue;
 
 public class ReplaceTilesTest {
     Placement a = new Placement();
-    //generate a new die
-    String[] die = {"A" + (int)(Math.random()*6), "B" + (int)(Math.random()*3), "S" + (int)(Math.random()*6)};
-    String[] row = {"A", "B", "C", "D", "E", "F", "G"};
+    //different types of dies:
+    String[] dies = {"S0", "S1", "S2", "A0", "A1", "A2", "A3", "A4", "A5", "B0", "B1", "B2"};
 
 
     @Rule
@@ -21,18 +21,19 @@ public class ReplaceTilesTest {
 
     @Test
     public void test0(){
-        String i = die[(int)(Math.random()*3)] + row[(int)(Math.random()*7)] + (int)(Math.random()*7) + "0";
-        //for (String i: r0){
+        for (String d: dies){
+            //to create a valid tile with 0 rotation.
+            String i = d +"A1"+"0";
             String index = i.substring(0, 4).concat("0");
             String temp1 = a.replace(index);
             String temp = a.replace(i);
             assertEquals(temp, temp1);
-        //}
+        }
     }
     @Test
     public void test90 (){
-        String i = die[(int)(Math.random()*3)] + row[(int)(Math.random()*7)] + (int)(Math.random()*7) + "1";
-        //for (String i: r90){
+        for (String d: dies){
+            String i = d + "A1" +"1";
             String index = i.substring(0, 4).concat("0");
             String temp1 = a.replace(index);
             String temp = a.replace(i);
@@ -45,14 +46,15 @@ public class ReplaceTilesTest {
                 }
                 char tempc = temp.charAt(j);
                 char ic = temp1.charAt(b);
-                assertTrue("Expected rotate 90 degrees, but got" + tempc, tempc == ic);
+                assertTrue("Expected rotate 90 degrees, but got" + tempc + " not equals to " + ic,
+                        tempc == ic);
             }
-        //}
+        }
     }
     @Test
     public void test180(){
-        String i = die[(int)(Math.random()*3)] + row[(int)(Math.random()*7)] + (int)(Math.random()*7) + "2";
-        //for (String i: r180){
+        for (String d: dies){
+            String i = d + "A1" +"2";
             String index = i.substring(0, 4).concat("0");
             String temp1 = a.replace(index);
             String temp = a.replace(i);
@@ -67,12 +69,12 @@ public class ReplaceTilesTest {
                 char tempc = temp.charAt(b);
                 assertTrue("Expected rotate 180 degrees, but got" + tempc, tempc == ic);
             }
-        //}
+        }
     }
     @Test
     public void test270(){
-        String i = die[(int)(Math.random()*3)] + row[(int)(Math.random()*7)] + (int)(Math.random()*7) + "3";
-        //for (String i: r270){
+        for (String d: dies){
+            String i = d + "A1" + "3";
             String index = i.substring(0, 4).concat("0");
             String temp1 = a.replace(index);
             String temp = a.replace(i);
@@ -87,23 +89,23 @@ public class ReplaceTilesTest {
                 char ic = temp1.charAt(b);
                 assertTrue("Expected rotate 270 degrees, but got" + tempc, tempc == ic);
             }
-        //}
+        }
     }
     @Test
     public void testc0(){
-        String i = die[(int)(Math.random()*3)] + row[(int)(Math.random()*7)] + (int)(Math.random()*7) + "4";
-        //for (String i: c0){
+        for (String d: dies){
+            String i = d + "A1" + "4";
             String index = i.substring(0, 4).concat("0");
             String temp1 = a.replace(index);
             String temp = a.replace(i);
             assertTrue("Expected left equals right side, but got" + temp.charAt(1) + ", not equal to " + temp1.charAt(3),
                     temp.charAt(1) == temp1.charAt(3));
-        //}
+        }
     }
     @Test
     public void testc90(){
-        String i = die[(int)(Math.random()*3)] + row[(int)(Math.random()*7)] + (int)(Math.random()*7) + "5";
-        //for (String i: c90){
+        for (String d: dies){
+            String i = d + "A1" + "5";
             String index = i.substring(0, 4).concat("0");
             String temp1 = a.replace(index);
             String temp = a.replace(i);
@@ -111,23 +113,23 @@ public class ReplaceTilesTest {
                     temp.charAt(0) == temp1.charAt(1));
             assertTrue("Expected down equals left side, but got" + temp.charAt(2) +  ", not equal to " + temp1.charAt(1),
                     temp.charAt(2) == temp1.charAt(3));
-        //}
+        }
     }
     @Test
     public void testc180(){
-        String i = die[(int)(Math.random()*3)] + row[(int)(Math.random()*7)] + (int)(Math.random()*7) + "6";
-        //for (String i: c180){
+        for (String d: dies){
+            String i = d + "A1" + "6";
             String index = i.substring(0, 4).concat("0");
             String temp1 = a.replace(index);
             String temp = a.replace(i);
             assertTrue("Expected up equals down side, but got" + temp.charAt(0) +"not equal to" + temp1.charAt(2),
                     temp.charAt(0) == temp1.charAt(2));
-        //}
+        }
     }
     @Test
     public void testc270(){
-        String i = die[(int)(Math.random()*3)] + row[(int)(Math.random()*7)] + (int)(Math.random()*7) + "7";
-        //for (String i: c270){
+        for (String d: dies){
+            String i = d + "A1" + "7";
             String index = i.substring(0, 4).concat("0");
             String temp1 = a.replace(index);
             String temp = a.replace(i);
@@ -135,31 +137,6 @@ public class ReplaceTilesTest {
                     temp.charAt(0) == temp1.charAt(3));
             assertTrue("Expected down equals right side, but got" + temp.charAt(1) + ", not equal to " + temp1.charAt(2),
                     temp.charAt(1) == temp1.charAt(2));
-        //}
+        }
     }
-
-//    static final String[] r0 = {
-//            "A4A10", "B2G50", "S3F40"
-//    };
-//    static final String[] r90 = {
-//            "A4B21", "B0A01", "S0C51"
-//    };
-//    static final String[] r180 = {
-//            "B0C32", "A4F42", "A0F32"
-//    };
-//    static final String[] r270 = {
-//            "A3B13", "B1C33", "S0E33"
-//    };
-//    static final String[] c0 = {
-//            "A5D34", "B1E44", "S4C34"
-//    };
-//    static final String[] c90 = {
-//            "S0B35", "B1G25", "A5A05"
-//    };
-//    static final String[] c180 = {
-//            "A5A06", "B1B06", "S4A06"
-//    };
-//    static final String[] c270 = {
-//            "S1B57", "B0E47", "A4B27"
-//    };
 }
