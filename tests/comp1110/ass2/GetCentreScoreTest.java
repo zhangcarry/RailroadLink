5e087b100ad02b83a13d2eb6ee84a01df2a23da8
carryzhang <u6499267@anu.edu.au>
2019-05-08 12:32:50 +1000

    Saving Progress

--- tests/comp1110/ass2/GetCentreScoreTest.java
+++ tests/comp1110/ass2/GetCentreScoreTest.java
@@ -4,6 +4,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 
+import java.util.Random;
+
 import static org.junit.Assert.assertTrue;
 
 public class GetCentreScoreTest {
@@ -16,7 +18,22 @@ public class GetCentreScoreTest {
         int num = RailroadInk.getCentreScore("A1A30A0B30A5A11B1B20S4A23");
         assertTrue("Expected centre tiles score of 0, but method returned " + num, num == 0);
     }
-
+    /*
+    @Test
+    public void testHalfNHalf() {
+        String[] dies = {"S0", "S1", "S2", "A0", "A1", "A2", "A3", "A4", "A5", "B0", "B1", "B2"};
+        Character[] centrecol = {'3', '4', '5'};
+        Character[] centrerow = {'C', 'D', 'E'};
+        String placement = "";
+        Random random = new Random();
+        for (int i = 0; i<30; i = i+5) {
+            String die = dies[random.nextInt(13)];
+            String position = centrecol[random.nextInt(4)] + centrerow[random.nextInt(4)] + "";
+            String ori = Integer.toString(random.nextInt(8));
+            placement = placement + die + position + ori;
+        }
+    }
+    */
     @Test
     public void testLongString() {
         int num = RailroadInk.getCentreScore("A0B22B1A61A4D11A4G10B1G44A2G30A3C01A3C12B0B31A1B01A4B50B0C50A2F32A0E32A0E40A4D31B1D47A1B11B0D61A2D50A3G50A4G61");
