6307e41cfc089e396fd2e59d103724838c2e9166
carryzhang <u6499267@anu.edu.au>
2019-05-15 11:07:48 +1000

    Additional Comments

--- src/comp1110/ass2/Game.java
+++ src/comp1110/ass2/Game.java
@@ -71,15 +71,29 @@ public class Game extends Application {
 
         }
 
+        /**
+         * Move the piece to the new position
+         * @param movementX & movementY distance the mouse has moved since its original position
+         */
+
         void drag(double movementX, double movementY) {
             setLayoutX(getLayoutX() + movementX);
             setLayoutY(getLayoutY() + movementY);
         }
 
+        /**
+         * Check if the piece is on the board
+         * @return true if piece is on board
+         */
+
         private boolean onBoard() {
             return getLayoutX() > (MARGIN) && (getLayoutX() < (WINDOW_WIDTH - MARGIN - BOARD_SIZE))
                     && getLayoutY() > (MARGIN) && (getLayoutY() < (WINDOW_HEIGHT - MARGIN));
         }
+
+        /**
+         * Return the piece to starting position
+         */
 
         private void snapToHome() {
             setImage(new Image(PIECE_LOCATION));
@@ -106,7 +120,9 @@ public class Game extends Application {
         grid.setLayoutY(MARGIN);
 
 
-        /* Railway and Highway exits*/
+        /**
+         * Railway and Highway Exits
+         */
 
         int[] HwyExitX = {210, 610, 210, 610, 40, 780};
         int[] HwyExitY = {40, 40, 780, 780, 410, 410};
